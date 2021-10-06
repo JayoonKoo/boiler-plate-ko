e2e225d6f356da16b5308f02f81c7856b953f5bb
JayoonKoo <rnwkdbs12@gmail.com>
2021-10-06 21:03:30 +0900

    feat: 로그인 구현

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
@@ -48,6 +49,25 @@ userSchema.pre('save', function(next) {
 	}
 	next();
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+	// plainPassword vs 암호화된 비밀번호, 암호화해서 비교해야 함. 복호화 불가능 
+	bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+		if (err) return cb(err) 
+			cb(null, isMatch)
+	})
+}
+
+userSchema.methods.generateToken = function(cb) {
+	const user = this;
+	// jsonwebtoken을 이요해서 toekn 생성하기 
+	const token = jwt.sign(user._id.toHexString(), 'secretToken');
+	user.token = token;
+	user.save(function(err, user) {
+		if (err) return cb(err);
+		cb(null, user);
+	});
+}
 
 const User = mongoose.model('User', userSchema);
 
